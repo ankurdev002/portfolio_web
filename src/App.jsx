c0f49116f2a2182a7d96b9d005d62e39b859a8b1
ankurdev002 <contact.dev.ankur@gmail.com>
2024-01-13 12:50:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Test from "./Test";
+import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Skills from "./components/skills/Skills";
+
+
+const App = () => {
+  return (
+    <div className="main-port-container">
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Skills">
+        <Parallax type="skills" />
+      </section>
+      <section>
+        <Skills />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,114 @@
+import { useRef } from "react";
+import "./skills.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Skills = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on growing my skills
+          <br /> and company growth
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>Faster</motion.b>{" "}
+            Development
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+            Company.
+          </h1>
+          <button>WHAT SKILLS DO I HAVE?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>HTML</h2>
+          <img src="/html.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>CSS</h2>
+          <img src="/css.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>JAVASCRIPT</h2>
+          <img src="/js.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>TYPESCRIPT</h2>
+          <img src="/ts.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>REACTJS</h2>
+          <img src="/react.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>NEXTJS</h2>
+          <img src="/next.png" />
+          {/* <button>Go</button> */}
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Skills;
